3c6000ec970bc184183668de1b71e1f5378261d0
KevinZheng99 <kevinzhengkz99@gmail.com>
2022-01-14 21:05:38 -0500

    Minor fixes/changes

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React, { useState } from "react";
 
 import Header from "./components/Navigation/Header";
 import AnimeList from "./components/Pages/AnimeList/AnimeList";
-import Menu from "./components/Navigation/Hamburger/Menu";
 import classes from "./App.module.css";
 
 function App() {
@@ -16,7 +15,6 @@ function App() {
     <div className={classes.app}>
       <Header onUserInput={getUserInput} />
       <AnimeList userSearch={userSearch} />
-      {/* <Menu /> */}
     </div>
   );
 }
